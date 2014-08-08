7fe6a384ac1212a55ebef4aa7a11eeaaec46bf47
SexySonic23 <paulrlutz@gmail.com>
2014-08-08 15:41:17 -0400

    Code review Pt1

--- MyoTest.py
+++ MyoTest.py
@@ -1,11 +1,19 @@
 from myo import Myo
+from threading import Thread
+from getpass import getpass
 
-    
+
 
 def main():
   myMyo = Myo()
-  myMyo.startThread()
+  startThread(myMyo)
  
-  
+def startThread(tempMyo):
+  t = Thread(target=tempMyo.processMyo, args=("Thread-1",2,))
+  t.daemon = True
+  t.start()
+  while True:
+    pass
+      
 if __name__ == "__main__":
     main()
--- myo.py
+++ myo.py
@@ -1,125 +1,109 @@
+# Communicates with PyMyo.exe to enable Python support of Myo armband
+# Paul Lutz
+# Scott Martin
+# Fabricate.IO
+
 import time
-from threading import Thread
-import os
 import subprocess
 import sys
 import struct
 import math
-from getpass import getpass
 
 class Myo:
+  POSES = {
+    0 : "rest",
+    1 : "fist",
+    2 : "waveIn",
+    3 : "waveOut",
+    4 : "fingersSpread",
+    5 : "reserved1",
+    6 : "thumbToPinky",
+  }
+
+  CMD = "PyMyo.exe"
+  
+  PACKET_LEN = 30
+  PACKET_FORMAT = "fffffffBB"
+  
   def __init__(self):
-    self.poses = {0 : "rest",
-                1 : "fist",
-                2 : "waveIn",
-                3 : "waveOut",
-                4 : "fingersSpread",
-                5 : "reserved1",
-                6 : "thumbToPinky",
-                7 : "unknown",
-                255 : "unknown",
-    }
+    pass
   
   def processMyo(self, threadName, delay):
-    cmd = "PyMyo.exe"
-    self.proc = subprocess.Popen(cmd, bufsize=0, stdout= subprocess.PIPE, stdin= subprocess.PIPE, shell=True)
-    self.pose = 255
+    self.proc = subprocess.Popen(Myo.CMD, bufsize=0, stdout= subprocess.PIPE, stdin= subprocess.PIPE, shell=True)
+    self.pose = None
+    self.quat = None
+    self.accel = None
+    
     while True:
+      self.last_pose = self.pose
+      
+      # This will hang until the next data event is read
       newdata = self.proc.stdout.readline().strip()
-      if len(newdata) == 30:
-        my_struct = struct.unpack("fffffffBB",newdata)
-        self.calculateEuler(my_struct[3],my_struct[4],my_struct[5],my_struct[6])
-        self.whichArm = my_struct[8]
-        self.lastpose = self.pose
-        self.pose = my_struct[7]
-        if (self.poses[self.pose] == "fist") and (self.poses[self.lastpose] != "fist"):
-          self.proc.stdin.write(chr(1) + "\n")
-          self.proc.stdin.flush()
-        self.printData()
-        #print self.pose
-        #print my_struct[8]
-        '''
-        for x in my_struct:
-          print x
-        '''
-  def calculateEuler(self, w, x, y, z):
-    self.roll = math.atan2(2*y*w - 2*x*z, 1 - 2*y*y - 2*z*z)
-    self.pitch = math.atan2(2*x*w - 2*y*z, 1 - 2*x*x - 2*z*z)
-    self.yaw = math.asin(2*x*y + 2*z*w)
-    
-    myPi = math.pi
-    self.roll_w = int(((self.roll +myPi)/(myPi*2.0))*18.0)
-    self.pitch_w = int(((self.pitch +myPi)/(myPi*2.0))*18.0)
-    self.yaw_w = int(((self.yaw +myPi)/(myPi*2.0))*18.0)
-    '''
-    self.roll_w = int((self.roll + myPi/2.0)/(myPi*18.0)
-    self.pitch_w = int((self.pitch + myPi/2.0)/myPi*18.0)
-    self.yaw_w = int((self.yaw + myPi/2.0)/myPi*18.0)
-    '''
-    
-  def startThread(self):
-    t = Thread(target=self.processMyo, args=("Thread-1",2,))
-    t.daemon = True
-    t.start()
-    while True:
-      self.input = getpass("")
-      sys.stdout.write("\033[5m")
       
+      if len(newdata) != Myo.PACKET_LEN:
+        continue
       
+      data = struct.unpack(Myo.PACKET_FORMAT,newdata)
+      (self.accel, self.quat, self.pose, self.which_arm) = (data[0:3], data[3:7], data[7], data[8])
       
-    
-    
-  
-  def printData(self):
-  
-    sys.stdout.write("\r")
+      if (Myo.POSES.get(self.pose) == "fist") and (self.last_pose != self.pose):
+        self.proc.stdin.write(chr(1) + "\n")
+        self.proc.stdin.flush()
+      self.printData()
         
-  #print roll
-    sys.stdout.write("[")
-    for x in range(0, 17):
-      if self.roll_w >= x:
-        sys.stdout.write("*")
-      else:
-        sys.stdout.write(" ")
-    sys.stdout.write("]")
+  def calculateEuler(self, quat):
+    (w, x, y, z) = quat
     
-    #print pitch
-    sys.stdout.write("[")
-    for x in range(0, 17):
-      if self.pitch_w >= x:
-        sys.stdout.write("*")
-      else:
-        sys.stdout.write(" ")
-    sys.stdout.write("]")
-    
-    #print yaw
-    sys.stdout.write("[")
-    for x in range(0, 17):
-      if self.roll_w >= x:
-        sys.stdout.write("*")
-      else:
-        sys.stdout.write(" ")
-    sys.stdout.write("]")
-    
-    #print arm
-    sys.stdout.write("[")
-    if self.whichArm == 0:
-      sys.stdout.write("R")
-    elif self.whichArm == 1:
-      sys.stdout.write("L")
+    if (1 - 2*y*y - 2*z*z !=0):
+      roll = math.atan2(2*y*w - 2*x*z, 1 - 2*y*y - 2*z*z)
     else:
-      sys.stdout.write("?")
-    sys.stdout.write("]")
+      roll = 0
+    if (1 - 2*y*y - 2*z*z !=0):
+      pitch = math.atan2(2*x*w - 2*y*z, 1 - 2*x*x - 2*z*z)
+    else:
+      pitch = 0
     
-    sys.stdout.write("[")
-    count = 0
-    for x in self.poses[self.pose]:
-      count += 1
-      sys.stdout.write(x)
-    for x in range(0, 15-count):
-      sys.stdout.write(" ")
-      
-    sys.stdout.write("]")
+    yaw = math.asin(2*x*y + 2*z*w)
     
+    return (roll, pitch, yaw)
+
+  def printData(self):
+    arm_str = {
+      0: "R",
+      1: "L"
+    }.get(self.which_arm, "?")
+     
+    # Rotation is represented by number of stars (as in hello-myo.exe)
+    (roll_str, pitch_str, yaw_str) = ["*" * int(r) for r in self.getRotationScaled(18.0)]
+   
+    pose_str = self.getPoseString()
     
+    # Print out the rotation and arm state on the same line each update
+    sys.stdout.write('\r[{:17s}][{:17s}][{:17s}][{:1s}][{:15s}]'.format(
+        roll_str,
+        pitch_str,
+        yaw_str,
+        arm_str, 
+        pose_str
+      )
+    )
     
+  def getAcceleration(self):
+    return self.accel
+   
+  def getRotation(self):
+    # Return roll, pitch, and yaw as an array
+    return self.calculateEuler(self.quat)
+   
+  def getRotationScaled(self, scale):
+    return [(x+math.pi)/(math.pi*2.0)*scale for x in self.getRotation()]
+   
+  def getArm(self):
+    return self.which_arm
+    
+  def getPoseString(self):
+    return self.POSES.get(self.pose, "unknown")
+    
+  def getPoseVal(self):
+    return self.pose
+  
