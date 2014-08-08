4c0dee8ab76ac4f0e2d8f8b65e247800c8be3c50
SexySonic23 <paulrlutz@gmail.com>
2014-08-08 16:48:55 -0400

    Code Review complete

--- MyoTest.py
+++ MyoTest.py
@@ -1,19 +1,42 @@
+# Test of PyMyo - same functionality as hello-myo.exe
+# Paul Lutz
+# Scott Martin
+
 from myo import Myo
-from threading import Thread
-from getpass import getpass
+import sys
 
+last_pose = None
 
+def printData(myo):
+  global last_pose
+  
+  # Rotation is represented by number of stars (as in hello-myo.exe)
+  (roll_str, pitch_str, yaw_str) = ["*" * int(r) for r in myo.getRotationScaled(18.0)]
+  
+  arm_str = myo.getArmString()
+  
+  pose_str = myo.getPoseString()
+  
+  # Print out the rotation and arm state on the same line each update
+  sys.stdout.write('\r[{:17s}][{:17s}][{:17s}][{:1s}][{:15s}]'.format(
+      roll_str,
+      pitch_str,
+      yaw_str,
+      arm_str, 
+      pose_str,
+    )
+  )
+  
+  if (pose_str == "fist") and (last_pose != myo.getPose()):
+    myo.vibrate(Myo.VIBE_MEDIUM)
+  
+  last_pose = myo.getPose()
 
 def main():
-  myMyo = Myo()
-  startThread(myMyo)
- 
-def startThread(tempMyo):
-  t = Thread(target=tempMyo.processMyo, args=("Thread-1",2,))
-  t.daemon = True
-  t.start()
-  while True:
-    pass
+  myMyo = Myo(callback=printData)
+  myMyo.daemon = True
+  myMyo.start()
+  raw_input("Press enter to exit")
       
 if __name__ == "__main__":
     main()
--- myo.py
+++ myo.py
@@ -8,8 +8,11 @@ import subprocess
 import sys
 import struct
 import math
+import threading
 
-class Myo:
+class Myo(threading.Thread):
+  """ Wrapper for PyMyo.exe - handles event data via callback, and can vibrate the myo """
+  
   POSES = {
     0 : "rest",
     1 : "fist",
@@ -19,91 +22,92 @@ class Myo:
     5 : "reserved1",
     6 : "thumbToPinky",
   }
+  
+  # These durations are specified by Thalmic
+  VIBE_LONG = 2
+  VIBE_MEDIUM = 1
+  VIBE_SHORT = 0
 
   CMD = "PyMyo.exe"
   
+  # PyMyo.exe prints a packet (followed by a newline) every time the myo has new event data
   PACKET_LEN = 30
   PACKET_FORMAT = "fffffffBB"
   
-  def __init__(self):
-    pass
-  
-  def processMyo(self, threadName, delay):
-    self.proc = subprocess.Popen(Myo.CMD, bufsize=0, stdout= subprocess.PIPE, stdin= subprocess.PIPE, shell=True)
+  def __init__(self, callback):
+    threading.Thread.__init__(self)
+    
+    self.proc = subprocess.Popen(Myo.CMD, bufsize=0, stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
+    
     self.pose = None
     self.quat = None
+    self.rot = None
     self.accel = None
     
+    self.callback = callback
+    
+  def run(self):
     while True:
-      self.last_pose = self.pose
-      
       # This will hang until the next data event is read
       newdata = self.proc.stdout.readline().strip()
-      
       if len(newdata) != Myo.PACKET_LEN:
         continue
       
+      # Extract data from the packet
       data = struct.unpack(Myo.PACKET_FORMAT,newdata)
       (self.accel, self.quat, self.pose, self.which_arm) = (data[0:3], data[3:7], data[7], data[8])
+   
+      # Update Euler rotation if valid
+      self.rot = self.calculateEuler(self.quat) or self.rot
       
-      if (Myo.POSES.get(self.pose) == "fist") and (self.last_pose != self.pose):
-        self.proc.stdin.write(chr(1) + "\n")
-        self.proc.stdin.flush()
-      self.printData()
+      # Callback to user code
+      self.callback(self)
         
   def calculateEuler(self, quat):
+    # Convert quaternion to (roll, pitch, yaw) rotation
     (w, x, y, z) = quat
-    
-    if (1 - 2*y*y - 2*z*z !=0):
+    try:
       roll = math.atan2(2*y*w - 2*x*z, 1 - 2*y*y - 2*z*z)
-    else:
-      roll = 0
-    if (1 - 2*y*y - 2*z*z !=0):
       pitch = math.atan2(2*x*w - 2*y*z, 1 - 2*x*x - 2*z*z)
-    else:
-      pitch = 0
-    
-    yaw = math.asin(2*x*y + 2*z*w)
-    
-    return (roll, pitch, yaw)
+      yaw = math.asin(2*x*y + 2*z*w)
+      return (roll, pitch, yaw)
+    except: 
+      return None
 
-  def printData(self):
-    arm_str = {
-      0: "R",
-      1: "L"
-    }.get(self.which_arm, "?")
-     
-    # Rotation is represented by number of stars (as in hello-myo.exe)
-    (roll_str, pitch_str, yaw_str) = ["*" * int(r) for r in self.getRotationScaled(18.0)]
-   
-    pose_str = self.getPoseString()
-    
-    # Print out the rotation and arm state on the same line each update
-    sys.stdout.write('\r[{:17s}][{:17s}][{:17s}][{:1s}][{:15s}]'.format(
-        roll_str,
-        pitch_str,
-        yaw_str,
-        arm_str, 
-        pose_str
-      )
-    )
+  def vibrate(self, duration):
+    # Vibrates the Myo (must be one of VIBE_*)
+    assert duration in [Myo.VIBE_LONG, Myo.VIBE_MEDIUM, Myo.VIBE_SHORT]
+    self.proc.stdin.write(chr(1) + "\n")
+    self.proc.stdin.flush()
     
   def getAcceleration(self):
+    # Return [x, y, z] acceleration
     return self.accel
    
   def getRotation(self):
-    # Return roll, pitch, and yaw as an array
-    return self.calculateEuler(self.quat)
+    # Return [roll, pitch, yaw]
+    return self.rot
    
   def getRotationScaled(self, scale):
-    return [(x+math.pi)/(math.pi*2.0)*scale for x in self.getRotation()]
+    # Returns [x, y, z] accel scaled between 0 and scale.
+    return [(x+math.pi)/(math.pi*2.0)*scale for x in self.rot]
    
   def getArm(self):
+    # Returns 0 for right arm, 1 for left arm
     return self.which_arm
     
-  def getPoseString(self):
-    return self.POSES.get(self.pose, "unknown")
+  def getArmString(self):
+    # Return "R" for right arm, "L" for left arm, or "?" if no arm
+    return {
+      0: "R",
+      1: "L"
+    }.get(self.getArm(), "?")
     
-  def getPoseVal(self):
+  def getPoseString(self):
+    # Return string representing pose, or "unknown" if unknown
+    return self.POSES.get(self.getPose(), "unknown")
+    
+  def getPose(self):
+    # Return integer pose state
     return self.pose
   
